ac4809622ea168b26587b2f5839a1042980af6d7
Ivan Shilin <68341349+IvShilin@users.noreply.github.com>
2021-12-21 18:58:58 +0300

    Full

---
+++ cmake-build-debug/CMakeFiles/dop.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dop.dir/main.cpp.obj"
+  "dop.exe"
+  "dop.exe.manifest"
+  "dop.pdb"
+  "libdop.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dop.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+#include <fstream>
+#include <windows.h>
+
+using namespace std;
+
+const char* FNAME = "input.txt";
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    SetConsoleOutputCP(CP_UTF8);
+
+    ifstream FIN(FNAME);
+
+    int i, j, temp,
+    k = 0,
+    minI, minV,
+    maxI, maxV,
+    leftBorderIndex,
+    rightBorderIndex;
+
+    //файл не найден
+    if (!FIN)
+    {
+        cout << "ОШИБКА! Файл " << FNAME << " не найден\n";
+        FIN.close(); //закрыть файл
+        system("pause");
+        return 1; //выход по ошибке
+    } // end if
+
+    //проверки состояния файла
+    if (FIN.peek() == EOF) 	//файл пуст
+    {
+        cout << "Файл " << FNAME << " пустой\n";
+        FIN.close();    //закрыть файл
+        system("pause");
+        return 2;   //выход по ошибке
+    } // end if
+
+    while(!FIN.eof()){ // цикл считывания файла до конца
+        FIN >> temp; // запись в переменную
+        k++; // инкриментируем счётчик
+
+        if (FIN.fail()) { ///в файле не число
+            cout << "Сбой при чтении файла " << FNAME << endl;
+            FIN.close();//закрыть файл
+            system("pause");
+            return 3;//выход по ошибке
+        }
+    }
+
+    int const N = k; // размерность исходного массива
+    int AVec[N]; // декларирование исходного массива
+
+    FIN.seekg(0); // перемещение указателя в начало считываемой строки
+
+    cout << "Полученный из файла " << FNAME << " массив (исходный): " << endl;
+    for (i = 0; i < N; i++){ // цикл инициализации исходного массива
+        FIN >> AVec[i];
+        cout << AVec[i] << " "; // вывод каждого элемента считанного массива
+    }
+
+    minV = maxV = AVec[0]; // инициализация минимума и максимума по первому элменту считанного массива
+
+    for (i = 0; i < N; i++){ // поиск минимума и максимума и их индексов
+        if (AVec[i] > maxV){
+            maxV = AVec[i];
+            maxI = i;
+        }
+        if (AVec[i] < minV){
+            minV = AVec[i];
+            minI = i;
+        }
+    }
+
+    cout << endl << endl;
+
+    cout << "Максимум: " << maxV << endl
+         << "Индекс максимума: " << maxI
+         << endl << endl;
+
+    cout << "Минимум: " << minV << endl
+         << "Индекс минимума: " << minI
+         << endl << endl;
+
+    if (maxI > minI){ // определение левой и правой границы
+        leftBorderIndex = minI;
+        rightBorderIndex = maxI;
+    } else {
+        leftBorderIndex = maxI;
+        rightBorderIndex = minI;
+    }
+
+    k = 0; // обнуление счётчика
+    for (i = leftBorderIndex + 1; i < rightBorderIndex - k; i++){ // цикл удаления
+        if (AVec[i] >= 0){
+            for (j = 0; j < ( (N - k) - i - 1); j++){
+                temp = AVec[i+j];
+                AVec[i+j] = AVec[i+j+1];
+                AVec[i+j+1] = temp;
+            }
+            i--;
+            k++;
+        }
+    }
+
+
+    cout << "Измененный массив с переставленными значениями: " << endl;
+    for (i = 0; i < N; i++){
+        cout << AVec[i] << " ";
+    }
+
+    cout << endl << endl << "Итоговый массив: " << endl;
+    for (i = 0; i < N - k; i++){
+        cout << AVec[i] << " ";
+    }
+    return 0;
+}
